d5e269d005ebd9f6153c9948862dfaa1214fdb09
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-17 18:25:55 -0400

    Added PUT request

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ const accounts = require('./data/accounts-model.js');
 
 // CREATE - add a new item
 server.post('/', (req, res) => {
-
+    const newAccount = { name: req.body.name, budget: req.body.budget };
+    accounts.add(newAccount)
+    .then(data => {
+        res.status(201).json(data);
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'The new account could not be added.' })
+    })
 })
 
 // READ - send a list back of all items
@@ -31,7 +38,21 @@ server.get('/', (req, res) => {
 })
 
 // UPDATE - update an item
-server.put('/', (req, res) => {
+server.put('/:id/', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    accounts.update(id, changes)
+    .then(data => {
+        if(data) {
+            res.json(data);
+        } else {
+            res.status(404).json({ error: 'Unable to updated account data.' })
+        }
+    })
+    .catch(error => {
+        res.status(500).send(code).json({ error: 'The account data could not be updated.' })
+    })
 
 })
 
