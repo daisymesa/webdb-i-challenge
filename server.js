922d70bc8bd83fc4afa85ad293b8fff9afe1fcd4
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-17 18:03:32 -0400

    Setting up functions needed for CRUD

--- server.js
+++ server.js
@@ -4,4 +4,33 @@ const server = express();
 
 // your code here
 
+server.get('/', (req,res) => {
+    res.send(`
+    <h2>Server is working!</h2>
+    `)
+})
+
+//////////////////// CRUD Endpoints ////////////////////
+
+// CREATE - add a new item
+server.post('/', (req, res) => {
+    
+})
+
+// READ - send a list back of all items
+server.get('/', (req, res) => {
+
+})
+
+// UPDATE - update an item
+server.put('/', (req, res) => {
+    
+})
+
+// DELETE/DESTROY - remove an item
+server.delete('/', (req, res) => {
+    
+})
+
+
 module.exports = server;
