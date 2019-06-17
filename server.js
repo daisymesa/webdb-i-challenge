c88475292564fab25861114b1964feac76b358f1
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-17 18:13:29 -0400

    Added GET request

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = express();
 
 // your code here
 
-server.get('/', (req,res) => {
+server.get('/', (req, res) => {
     res.send(`
     <h2>Server is working!</h2>
     `)
@@ -16,22 +16,28 @@ const accounts = require('./data/accounts-model.js');
 
 // CREATE - add a new item
 server.post('/', (req, res) => {
-    
+
 })
 
 // READ - send a list back of all items
 server.get('/', (req, res) => {
-
+    accounts.find()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'The list of accounts could not be retrieved.' })
+        })
 })
 
 // UPDATE - update an item
 server.put('/', (req, res) => {
-    
+
 })
 
 // DELETE/DESTROY - remove an item
 server.delete('/', (req, res) => {
-    
+
 })
 
 
